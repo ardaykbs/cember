7351d2fc79009c5f6fb5dcac875a6a75bf30a48d
Arda Y. <ardaykbs22@gmail.com>
2025-05-02 00:19:30 +0300

    yeni dosya eklendi.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        circle c1 = new circle();
+        circle c2 = new circle(6);
+      
+        System.out.println(c1);
+        System.out.println(c2);
+
+    }
+}
